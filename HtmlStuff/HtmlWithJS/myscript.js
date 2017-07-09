157e0360e9cdf297aba421262d8f3cb3e48b885d
ramtinj <ramtinj@kth.se>
2017-07-09 15:25:04 +0200

    learning some DOM interaction

---
+++ HtmlStuff/HtmlWithJS/myscript.js
@@ -0,0 +1,20 @@
+// some quick code where i practice DOM interaction with JS
+var header = document.querySelector("h1")
+
+function getRandomColor(){
+  var letters = "0123456789ABCDEF";
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random()*16)];
+  }
+  return color
+}
+
+// Simple function for clarity
+function changeHeaderColor(){
+  colorInput = getRandomColor()
+  header.style.color = colorInput;
+}
+
+// Now perform the action over intervals (milliseocnds):
+setInterval("changeHeaderColor()",500);
